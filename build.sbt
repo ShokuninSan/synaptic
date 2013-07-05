5d7927c59be62794fcc1f1e86e50e6f208ef0e3c
Shokunin San <sanshokunin@gmail.com>
2013-07-05 17:12:51 +0200

    Added unittests and build.sbt with dependencies.

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "Perceptron"
+
+version := "0.0.1"
+
+scalaVersion := "2.10.0"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.1.0",
+  "org.scalatest" %% "scalatest" % "1.9.1" % "test",
+  "junit" % "junit" % "4.10" % "test"
+)
---
+++ src/test/scala/perceptron/PerceptronSpec.scala
@@ -0,0 +1,91 @@
+package perceptron
+
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import scala.util.Random
+import org.scalatest.matchers.ShouldMatchers
+
+@RunWith(classOf[JUnitRunner])
+class PerceptronSpec extends FlatSpec with ShouldMatchers {
+
+  "A Perceptron" should "be able to solve XOR" in {
+
+    // The index of the list represents the layer # and the
+    // integer value the amount of neurons in that layer.
+    val net = new Perceptron(List(2,3,2,1), new Random)
+
+    // training
+    for (i <- 1 to 150) {
+      net.train(List(1.0, 1.0), List(-1.0))
+      net.train(List(-1.0, -1.0), List(-1.0))
+      net.train(List(1.0, -1.0), List(1.0))
+      net.train(List(-1.0, 1.0), List(1.0))
+    }
+
+    // run
+    net.run(List(1.0, 1.0)).head should be (-1.0 plusOrMinus 0.15)
+    net.run(List(-1.0, 1.0)).head should be (1.0 plusOrMinus 0.15)
+    net.run(List(1.0, -1.0)).head should be (1.0 plusOrMinus 0.15)
+    net.run(List(-1.0, -1.0)).head should be (-1.0 plusOrMinus 0.15)
+  }
+
+  it should "be able to solve OR" in {
+
+    val net = new Perceptron(List(2,3,2,1), new Random)
+
+    // training
+    for (i <- 1 to 150) {
+      net.train(List(1.0, 1.0), List(1.0))
+      net.train(List(1.0, -1.0), List(1.0))
+      net.train(List(-1.0, 1.0), List(1.0))
+      net.train(List(-1.0, -1.0), List(-1.0))
+    }
+
+    // run
+    net.run(List(1.0, 1.0)).head should be (1.0 plusOrMinus 0.15)
+    net.run(List(-1.0, 1.0)).head should be (1.0 plusOrMinus 0.15)
+    net.run(List(1.0, -1.0)).head should be (1.0 plusOrMinus 0.15)
+    net.run(List(-1.0, -1.0)).head should be (-1.0 plusOrMinus 0.15)
+  }
+
+
+  it should "be able to solve AND" in {
+
+    val net = new Perceptron(List(2,3,2,1), new Random)
+
+    // training
+    for (i <- 1 to 150) {
+      net.train(List(1.0, 1.0), List(1.0))
+      net.train(List(1.0, -1.0), List(-1.0))
+      net.train(List(-1.0, 1.0), List(-1.0))
+      net.train(List(-1.0, -1.0), List(-1.0))
+    }
+
+    // run
+    net.run(List(1.0, 1.0)).head should be (1.0 plusOrMinus 0.15)
+    net.run(List(-1.0, 1.0)).head should be (-1.0 plusOrMinus 0.15)
+    net.run(List(1.0, -1.0)).head should be (-1.0 plusOrMinus 0.15)
+    net.run(List(-1.0, -1.0)).head should be (-1.0 plusOrMinus 0.15)
+  }
+
+  it should "be able to solve NOT" in {
+
+    val net = new Perceptron(List(2,3,2,1), new Random)
+
+    // training
+    for (i <- 1 to 150) {
+      net.train(List(1.0, 1.0), List(-1.0))
+      net.train(List(1.0, -1.0), List(-1.0))
+      net.train(List(-1.0, 1.0), List(-1.0))
+      net.train(List(-1.0, -1.0), List(1.0))
+    }
+
+    // run
+    net.run(List(1.0, 1.0)).head should be (-1.0 plusOrMinus 0.15)
+    net.run(List(-1.0, 1.0)).head should be (-1.0 plusOrMinus 0.15)
+    net.run(List(1.0, -1.0)).head should be (-1.0 plusOrMinus 0.15)
+    net.run(List(-1.0, -1.0)).head should be (1.0 plusOrMinus 0.15)
+  }
+}
