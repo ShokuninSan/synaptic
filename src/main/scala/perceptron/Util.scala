32b7a237c089025189e10fdcf5555348ff6c9818
Shokunin San <me@flatmap.io>
2014-01-05 18:39:52 +0100

    Minor optimization.

--- src/main/scala/perceptron/Util.scala
+++ src/main/scala/perceptron/Util.scala
@@ -3,12 +3,11 @@ package perceptron
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import scala.util.Random
-import scala.math._
 
 object Util {
 
   def await(future: Future[List[Double]]): List[Double] = Await.result(future, Duration.Inf)
 
-  def randomWeight = (new Random).nextDouble * 2.0 - 1.0
+  def randomWeight = (new Random).nextDouble * 2. - 1.
 
 }
